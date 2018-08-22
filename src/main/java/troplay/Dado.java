32e9de3a872b6daba690a030f4e4554e58805dfa
Alejandro <you@fakemail.com>
2018-08-22 21:15:59 +0100

    Deleted comments

--- src/main/java/troplay/Dado.java
+++ src/main/java/troplay/Dado.java
@@ -3,34 +3,20 @@ package troplay;
 import java.util.Date;
 import java.util.Random;
 
-/**
- * La clase dado se encarga de generar un número aleatorio entre 1 y 6
- * @author alejandro
- */
-public class Dado extends Dibujable{
+public class Dado extends Dibujable {
     private Random rnd;
     private int valor = 0;
 
-    /**
-     * Inicializa la clase con una semilla diferente cada vez para
-     * que no se repitan las secuencias de números
-     */
     public Dado() {
         rnd = new Random();
         Date fecha = new Date();
         rnd.setSeed(fecha.getTime());
     }
-    
-    /**
-     * Devuelve el valor actual del dado
-     * @return Valor del dado
-     */
-    public int getValor() {return valor + 1;}
-    
-    /**
-     * Se encarga de generar un valor resultado de lanzar el dado
-     * @return El valor generado por el dado virtual
-     */
+
+    public int getValor() {
+        return valor + 1;
+    }
+
     public int getNuevoValor() {
         if((valor = rnd.nextInt() % 6) < 0) valor *= -1;
         return valor + 1;
